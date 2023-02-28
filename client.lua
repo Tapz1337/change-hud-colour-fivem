4b31ca28048a390e19615642567c84c46ca2f1ac
TapzQ <109795631+TapzQ@users.noreply.github.com>
2023-02-28 11:00:44 +0100

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,9 @@
+Citizen.CreateThread(function()
+    ReplaceHudColourWithRgba(
+      116, -- Nuber for the Old, Standard Colour
+      187, -- Number for Red
+      187, -- Number for Green
+      187, -- Number for Blue
+      255, -- Number for a
+    )
+end)
